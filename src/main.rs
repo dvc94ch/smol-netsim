1fb1e245647a4d2357bef80d298ccf09e56c2a2f
David Craven <david@craven.ch>
2020-07-08 00:33:59 +0200

    Blocking netsim.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 use crate::iface::Iface;
-use futures::channel::mpsc::{self, UnboundedReceiver, UnboundedSender};
-use futures::future::Future;
-use futures::io::{AsyncReadExt, AsyncWriteExt};
-use futures::sink::SinkExt;
-use futures::stream::StreamExt;
 use std::fs::File;
 use std::io::Write;
 use std::net::{Ipv4Addr, SocketAddrV4, UdpSocket};
+use std::sync::mpsc::{self, Receiver, Sender};
+use std::sync::Arc;
 use std::{io, thread};
 
 mod iface;
@@ -31,58 +28,49 @@ pub fn unshare_user() -> Result<(), io::Error> {
     Ok(())
 }
 
-pub fn machine<F>(
+pub fn machine<F, R>(
     addr: Ipv4Addr,
     mask: u8,
-    mut tx: UnboundedSender<Vec<u8>>,
-    mut rx: UnboundedReceiver<Vec<u8>>,
+    tx: Sender<Vec<u8>>,
+    rx: Receiver<Vec<u8>>,
     task: F,
-) -> thread::JoinHandle<F::Output>
+) -> thread::JoinHandle<R>
 where
-    F: Future + Send + 'static,
-    F::Output: Send + 'static,
+    F: FnOnce() -> R + Send + 'static,
+    R: Send + 'static,
 {
     thread::spawn(move || {
         unsafe {
             errno!(libc::unshare(libc::CLONE_NEWNET | libc::CLONE_NEWUTS)).unwrap();
         }
-        let iface = Iface::new().unwrap();
+        let iface = Arc::new(Iface::new().unwrap());
         iface.set_ipv4_addr(addr, mask).unwrap();
         iface.put_up().unwrap();
-        let iface = smol::Async::new(iface).unwrap();
-        let (mut reader, mut writer) = iface.split();
+        let iface2 = iface.clone();
 
-        smol::run(async move {
-            smol::Task::local(async move {
-                loop {
-                    let mut buf = [0; libc::ETH_FRAME_LEN as usize];
-                    let n = reader.read(&mut buf).await.unwrap();
-                    if n == 0 {
-                        break;
-                    }
-                    if tx.send(buf[..n].to_vec()).await.is_err() {
-                        break;
-                    }
+        thread::spawn(move || loop {
+            let mut buf = [0; libc::ETH_FRAME_LEN as usize];
+            let n = iface.recv(&mut buf).unwrap();
+            if n == 0 {
+                break;
+            }
+            if tx.send(buf[..n].to_vec()).is_err() {
+                break;
+            }
+        });
+
+        thread::spawn(move || loop {
+            if let Ok(packet) = rx.recv() {
+                let n = iface2.send(&packet).unwrap();
+                if n == 0 {
+                    break;
                 }
-            })
-            .detach();
+            } else {
+                break;
+            }
+        });
 
-            smol::Task::local(async move {
-                loop {
-                    if let Some(packet) = rx.next().await {
-                        let n = writer.write(&packet).await.unwrap();
-                        if n == 0 {
-                            break;
-                        }
-                    } else {
-                        break;
-                    }
-                }
-            })
-            .detach();
-
-            task.await
-        })
+        task()
     })
 }
 
@@ -90,31 +78,30 @@ fn main() {
     unshare_user().unwrap();
     let a_addr: Ipv4Addr = "192.168.1.5".parse().unwrap();
     let b_addr = "192.168.1.6".parse().unwrap();
-    let (a_tx, b_rx) = mpsc::unbounded();
-    let (b_tx, a_rx) = mpsc::unbounded();
+    let (a_tx, b_rx) = mpsc::channel();
+    let (b_tx, a_rx) = mpsc::channel();
 
-    let join1 = machine(a_addr.clone(), 24, a_tx, a_rx, async move {
-        let socket = smol::Async::<UdpSocket>::bind("0.0.0.0:3000").unwrap();
+    let join1 = machine(a_addr.clone(), 24, a_tx, a_rx, move || {
+        let socket = UdpSocket::bind("0.0.0.0:3000").unwrap();
         loop {
             let mut buf = [0u8; 11];
-            let (len, addr) = socket.recv_from(&mut buf).await.unwrap();
+            let (len, addr) = socket.recv_from(&mut buf).unwrap();
             if &buf[..len] == b"ping" {
                 println!("received ping");
-                socket.send_to(b"pong", addr).await.unwrap();
+                socket.send_to(b"pong", addr).unwrap();
                 break;
             }
         }
     });
 
-    let join2 = machine(b_addr, 24, b_tx, b_rx, async move {
-        let socket = smol::Async::<UdpSocket>::bind("0.0.0.0:3000").unwrap();
+    let join2 = machine(b_addr, 24, b_tx, b_rx, move || {
+        let socket = UdpSocket::bind("0.0.0.0:3000").unwrap();
         socket
             .send_to(b"ping", SocketAddrV4::new(a_addr, 3000))
-            .await
             .unwrap();
 
         let mut buf = [0u8; 11];
-        let (len, _addr) = socket.recv_from(&mut buf).await.unwrap();
+        let (len, _addr) = socket.recv_from(&mut buf).unwrap();
         if &buf[..len] == b"pong" {
             println!("received pong");
         }
